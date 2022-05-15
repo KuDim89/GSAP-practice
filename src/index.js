7a97ec764fc4e03f660fee58693c6bd6d4ae8cb1
KuDim89 <36006135+KuDim89@users.noreply.github.com>
2022-05-15 22:33:50 +0300

    was added simple parallax section

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 import './styles/styles.scss'
 
+import {SimpleParallax} from "@/js/simple-parallax";
+
+gsap.registerPlugin(ScrollTrigger);
+SimpleParallax();
